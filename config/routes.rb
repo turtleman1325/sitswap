bf003b54997893c407ca0db0e94c292441a9f689
Adam <adamjthayer@gmail.com>
2015-07-01 10:04:43 -0700

    Modified Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'welcome#index '
+  get "about" => "welocme#about", as: :about
+  get "contact" => "welocme#contact", as: :contact
+  get "log in" => "login#login", as: :log_in
   root 'welcome#index'
+  devise_for :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
