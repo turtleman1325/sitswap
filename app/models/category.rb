7c31e5493940760ebfb9c01d74ef19e6cd3c03d3
Tania Kelvin <tkdegrees@gmail.com>
2015-07-09 19:02:23 -0700

    models/pet validations, edit models/category, edit views/pets

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-  belongs_to :pets
+  has_and_belongs_to_many :pets
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,13 @@
 class Pet < ActiveRecord::Base
-  belongs_to :categories
+  has_and_belongs_to_many :categories
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :breed, presence: true, length: { minimum: 3 }
+  validates :color, presence: true, length: { minimum: 3 }
+  validates :birth_year, presence: true, length: { minimum: 4 }
+  validates :feeding, presence: true, length: { minimum: 2 }
+  validates :medications, presence: true, length: { minimum: 2 }
+  validates :vaccination_medical_notes, presence: true, length: { minimum: 4 }
+  validates :temperament, presence: true, length: { minimum: 2 }
+  validates :notes, presence: true, length: { minimum: 2 }
 end
