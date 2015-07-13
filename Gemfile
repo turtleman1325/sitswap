57089d1dc89dd172ad0714bf9e2ccf32a5b39bce
Tania Kelvin <tkdegrees@gmail.com>
2015-07-13 11:58:49 -0700

    link from nav to profile pets (index),alpha categories

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'devise', '~> 3.5.1'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
@@ -44,4 +44,6 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  gem 'materialize-sass'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701172258) do
+ActiveRecord::Schema.define(version: 20150702172532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150701172258) do
     t.datetime "updated_at"
     t.integer  "category_id"
   end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
